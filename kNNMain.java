6a930bd91d6f8ca40cb92015e7022243e5e76f15
jesspizz <jesspizz66@gmail.com>
2019-05-13 21:47:49 -0400

    Fix incomplete merge

--- kNNMain.java
+++ kNNMain.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 
 public class kNNMain{
 
-<<<<<<< HEAD
     public static void main(String... args) throws FileNotFoundException{
 
         // TASK 1: Use command line arguments to point DataSet.readDataSet method to
@@ -36,78 +35,46 @@ public class kNNMain{
         
         // TASK 5: Use the KNNClassifier class to determine the k nearest neighbors to a given DataPoint,
         // and make it print a predicted target label
-	KNNClassifier classifier = new KNNClassifier(5);
+        KNNClassifier classifier = new KNNClassifier(5);
         String predictedLabel = classifier.predict(dataset, myPoint);
         System.out.println("Predicted Label: " + predictedLabel);
 
         // TASK 6: loop over the datapoints in the held out test set, and make predictions for Each
         // point based on nearest neighbors in training set. Calculate accuracy of model.
-	int correctLabels = 0;
-	for(DataPoint p : testSet){
-	    String prediction = classifier.predict(dataset, p);
-	    if(prediction.equals(p.getLabel())) {
-		correctLabels++;
-	    }
-	}
-	System.out.println("Accuracy: " + ((double)correctLabels / testSet.size()) * 100);
+        int correctLabels = 0;
+        for(DataPoint p : testSet){
+            String prediction = classifier.predict(dataset, p);
+            if(prediction.equals(p.getLabel())) {
+                correctLabels++;
+            }
+        }
+        System.out.println("Accuracy: " + ((double)correctLabels / testSet.size()) * 100);
     }
-=======
-  public static void main(String... args) throws FileNotFoundException{
 
-    // TASK 1: Use command line arguments to point DataSet.readDataSet method to
-    // the desired file. Choose a given DataPoint, and print its features and label
+    public static double mean(double[] arr){
+        /*
+          Method that takes as an argument an array of doubles
+          Returns: average of the elements of array, as a double
+        */
+        double sum = 0.0;
 
-
-
-    //TASK 2:Use the DataSet class to split the fullDataSet into Training and Held Out Test Dataset
-
-
-    // TASK 3: Use the DataSet class methods to plot the 2D data (binary and multi-class)
-
-
-
-    // TASK 4: write a new method in DataSet.java which takes as arguments to DataPoint objects,
-    // and returns the Euclidean distance between those two points (as a double)
-
-
-
-    // TASK 5: Use the KNNClassifier class to determine the k nearest neighbors to a given DataPoint,
-    // and make a print a predicted target label
-
-
-
-    // TASK 6: loop over the datapoints in the held out test set, and make predictions for Each
-    // point based on nearest neighbors in training set. Calculate accuracy of model.
-
-
-  }
-
-  public static double mean(double[] arr){
-    /*
-    Method that takes as an argument an array of doubles
-    Returns: average of the elements of array, as a double
-    */
-    double sum = 0.0;
-
-    for (double a : arr){
-      sum += a;
+        for (double a : arr){
+            sum += a;
+        }
+        return (double)sum/arr.length;
     }
-    return (double)sum/arr.length;
-  }
 
-  public static double standardDeviation(double[] arr){
-    /*
-    Method that takes as an argument an array of doubles
-    Returns: standard deviation of the elements of array, as a double
-    Dependencies: requires the *mean* method written above
-    */
-    double avg = mean(arr);
-    double sum = 0.0;
-    for (double a : arr){
-      sum += Math.pow(a-avg,2);
+    public static double standardDeviation(double[] arr){
+        /*
+          Method that takes as an argument an array of doubles
+          Returns: standard deviation of the elements of array, as a double
+          Dependencies: requires the *mean* method written above
+        */
+        double avg = mean(arr);
+        double sum = 0.0;
+        for (double a : arr){
+            sum += Math.pow(a-avg,2);
+        }
+        return (double)sum/arr.length;
     }
-    return (double)sum/arr.length;
-  }
-
->>>>>>> ac8b64ec5ad33c35bb9f6ee98f2b36d7a3b0f37c
 }
